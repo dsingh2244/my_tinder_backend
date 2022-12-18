4f77ab88ea0ccfc0769fb7ea5e50933c4e5f6ea0
Digvijay singh <dsinght4@gmail.com>
2022-12-19 01:39:03 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import Cors from 'Cors';
+import Cards from './dbcards.js';
+
+const app= express();
+const port = process.env.PORT || 8001 ;
+
+
+main().catch(err => console.log(err));
+async function main() {
+    await mongoose.connect(`mongodb+srv://admin:Dtainugv@cluster0.iy7ieva.mongodb.net/?retryWrites=true&
+    w=majority`); 
+}
+
+app.use(express.json());
+app.use(Cors());
+app.get('/',(req,res)=> res.status(200).send('Hello'));
+app.post('/tinder/cards',(req,res)=>{
+    const dbCard= req.body;
+    Cards.create( dbCard, (err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(201).send(data)
+        }
+    });
+});
+
+app.get('/tinder/cards',(req,res)=>{
+    Cards.find((err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(200).send(data)
+        }
+    });
+});
+
+app.listen(port,()=> console.log(`listening on localhost: ${port}`));
